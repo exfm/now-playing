e2b2a06ac8d72407c391c0bd9477cab2f9d38b02
Dan Kantor <dan@ex.fm>
2012-09-24 15:52:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# now-playing
+
+
+
+
+## Install
+
+
+     npm install now-playing
+
+## Testing
+
+    git clone 
+    npm install
+    open test/index.html
---
+++ test/now-playing.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("now-playing", function(){
+    it("should have at least one test", function(){
+        assert.fail("Write a test!");
+    });
+});
